14b89925b6cf0c7999c8023546ec9c30ebfa1e77
Damian Andrysiak <andrydamian@gmail.com>
2020-07-22 15:18:31 +0200

    up

---
+++ Junction_Simulator_CMD/Junction_Simulator_CMD/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Junction_Simulator_CMD
+{
+    class Car
+    {
+        private int car_id;
+        private String car_name;
+
+        public Car()
+        {
+            this.car_id = 0;
+            this.car_name = "Renault";
+        }
+
+        public Car(int car_id, String car_name)
+        {
+            this.car_id = car_id;
+            this.car_name = car_name;
+        }
+
+
+
+        public string Car_name { get => car_name; set => car_name = value; }
+        public int Car_id { get => car_id; set => car_id = value; }
+    }
+}
---
+++ Junction_Simulator_CMD/Junction_Simulator_CMD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Junction_Simulator_CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 10;
+
+            //int ab = new int();
+            Draw_Junction Obj = new Draw_Junction(100,20);
+            Obj.Draw();
+        }
+    }
+}
